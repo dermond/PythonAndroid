64d6df984aaef595c40836046a3588b48fb840e8
DESKTOP-D3AON3N\User <dermond3336@gmail.com>
2025-06-16 10:52:42 +0800

    add ver

--- WebCrawler/WebCrawler/Newshopee.py
+++ WebCrawler/WebCrawler/Newshopee.py
@@ -503,7 +503,7 @@ if __name__ == '__main__':
 
   device, client = connect(deviceid)
   device_id = device.serial
-  jump = 100
+  jump = 70
   Leftspace = 0
   dpi = 10
 
@@ -570,12 +570,12 @@ if __name__ == '__main__':
         swipe_end = '500 100'
         swipe_to_position(device, swipe_start, swipe_end)  # 确保屏幕滚动到固定位置
         time.sleep(6.0)
-        jump = 100
+        jump = 70
         Shopeecount = Shopeecount + 1
         ErrorCount = 0
     elif result == "ok":
         Shopeecount = 0
-        jump = 100
+        jump = 70
         ErrorCount = 0
         turn_on_screen()
         continue
